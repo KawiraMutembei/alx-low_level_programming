63a503ceaface3ca376b2a44bf3a8d72fd9b3390
KawiraMutembei <kawiraneddy@gmail.com>
2022-06-05 00:30:23 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 
 /**
- *
- * * main-Entry point
- *
- * * Return: 0 if successful
- *
- * */
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
 
 int main(void)
 
